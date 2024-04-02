162b02dccd444b44c375d527011d0ac31fba1e78
Ambroise Maupate <ambroise@rezo-zero.com>
2024-04-02 19:46:23 +0200

    fix: Missing nitropack imports for server plugin

--- src/runtime/server/plugins/cache-control.ts
+++ src/runtime/server/plugins/cache-control.ts
@@ -1,11 +1,21 @@
+import type { NitroApp } from 'nitropack'
 import type { CacheControlOptions } from '~/src/runtime/composables/use-cache-control'
+import { getResponseStatus, setResponseHeader, getQuery, parseCookies } from 'h3'
+import { useRuntimeConfig } from 'nitropack/runtime'
+
+// type stub
+type NitroAppPlugin = (nitroApp: NitroApp) => void
+
+function defineNitroPlugin(def: NitroAppPlugin): NitroAppPlugin {
+    return def
+}
 
 /*
  * This plugin is used to set the cache control headers for the response.
  * It uses the useCacheControl composable to define cache control options.
  * It also checks for cookies and response status to determine if the response should be cached.
  */
-export default defineNitroPlugin((nitroApp) => {
+export default defineNitroPlugin((nitroApp: NitroApp) => {
     nitroApp.hooks.hook('render:response', (_response, { event }) => {
         const qs = getQuery(event)
 
